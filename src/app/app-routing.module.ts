61036ec51a98b555364431779929fd866b8af5e5
William Hendricks <wshendricks@gmail.comgit config --global user.email wshendricks@gmail.comgit config --global user.email wshendricks@gmail.com>
2018-06-23 19:21:22 -0500

    added forRoot initialization for router, AddEntry path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEntryComponent } from './add-entry/add-entry.component'
+
+const routes: Routes = [
+  { path: 'addentry', component: AddEntryComponent }
+];
 
 @NgModule({
-    exports: [ RouterModule ]
+    exports: [ RouterModule ],
+    imports: [ RouterModule.forRoot(routes) ]
 })
 
 export class AppRoutingModule { }
