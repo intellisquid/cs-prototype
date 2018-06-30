8c98ef8c46049bf7191cfd8929e9e1f00572eb44
William Hendricks <wshendricks@gmail.comgit config --global user.email wshendricks@gmail.comgit config --global user.email wshendricks@gmail.com>
2018-06-30 07:32:56 -0500

    added bootstrap@latest. imported FormsModule. editing add-entry.component.html (not finished, confused about how angular might name something)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AddEntryComponent } from './add-entry/add-entry.component';
@@ -8,6 +9,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
